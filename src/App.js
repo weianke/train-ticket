4dba05c2825a54010e3cabdfbfbc7d780a98d3bd
anke <245783065@qq.com>
2019-12-29 17:07:32 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -4,23 +4,64 @@ import './App.css'
 
 function App(props) {
   const [count, setCount] = useState(() => {
-    return props.defaultCount || 0;
-  });
+    return props.defaultCount || 0
+  })
   const [name, setName] = useState('anke')
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight
+  })
+
+  const onResize = () => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
+    })
+  }
 
   useEffect(() => {
-     setName('ankeF' + count)
+    setName('ankeF' + count)
+    document.title = count
   }, [count])
 
+  useEffect(() => {
+    window.addEventListener('resize', onResize, false)
+    return () => {
+      window.removeEventListener('resize', onResize, false)
+    }
+  }, [])
+
+  const onClick = () => {
+    console.log('click');
+  }
+
+  useEffect(() => {
+    document.querySelector('#size').addEventListener('click', onClick)
+    return () => {
+       document.querySelector('#size').removeEventListener('click', onClick)
+    }
+  })
+
   return (
-    <buton 
-      type="button"
-      onClick={() => {
-        setCount(count + 1)
-      }}
-    >
-      Click ({count}), {name}
-    </buton>
+    <div>
+      <buton
+        type="button"
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Click ({count}), {name}
+      </buton>
+      {count % 2 ? (
+        <span id="size">
+          size {size.width},{size.height}
+        </span>
+      ) : (
+        <p id="size">
+          size {size.width},{size.height}
+        </p>
+      )}
+    </div>
   )
 }
 
